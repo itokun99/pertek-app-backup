66cfd3ca2066b9a34835b34fb644167322470e3e
Syamsul Muttaqin <lombok.oc@gmail.com>
2022-08-31 12:56:38 +0700

    todo: notification popover data

--- src/components/appbar/NotificationPopover.tsx
+++ src/components/appbar/NotificationPopover.tsx
@@ -1,6 +1,18 @@
-import { Box, Popover, PopoverProps, Typography } from '@mui/material';
+import {
+  Box,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Popover,
+  PopoverProps,
+  Typography,
+} from '@mui/material';
 import Divider from '../Divider';
 import { popoverDefaultProps } from './AppBar';
+
+interface NotificationList {}
 
 const NotificationPopover = (props: PopoverProps) => {
   return (
@@ -11,6 +23,16 @@ const NotificationPopover = (props: PopoverProps) => {
           <Typography>Terdapat 10 notifikasi belum dibaca</Typography>
         </Box>
         <Divider />
+        <Box>
+          <List>
+            <ListSubheader>BARU</ListSubheader>
+            {[1, 2, 3, 4, 5, 10].map((_, key) => (
+              <ListItemButton key={key}>
+                <Typography>hello</Typography>
+              </ListItemButton>
+            ))}
+          </List>
+        </Box>
       </Box>
     </Popover>
   );

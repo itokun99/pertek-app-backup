13af457cbe3e9a1c1c48577668ca667f1a6d893a
Syamsul Muttaqin <lombok.oc@gmail.com>
2022-09-21 15:43:13 +0700

    fix: list loading skeleton

--- src/components/loader/ListLoader.tsx
+++ src/components/loader/ListLoader.tsx
@@ -10,7 +10,7 @@ export const ListLoader = ({ group, list }: ListLoaderProps) => {
   return (
     <Stack direction='column'>
       <Skeleton sx={{ width: 200, mb: 2 }} />
-      <Stack direction='column'>}
+      <Stack direction='column'>
         <Box display='flex' width='100%' height={30} justifyContent='center' alignItems='center'>
           <Skeleton width={15} height={25} sx={{ borderRadius: 1, ml: 1, mr: 2.6 }} />
           <Skeleton width='100%' height={25} sx={{ borderRadius: 1 }} />

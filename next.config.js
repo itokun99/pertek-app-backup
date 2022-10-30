aa6c7394e001c95eb3f9a7de43afd669cc8e7456
Syamsul Muttaqin <lombok.oc@gmail.com>
2022-10-30 21:35:44 +0700

    fix: config with PWA and bundle analyzer

--- next.config.js
+++ next.config.js
@@ -3,11 +3,9 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
 });
 
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-
-const shouldAnalyzeBundle = process.env.ANALYZE === 'true';
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -16,27 +14,6 @@ const nextConfig = {
   images: {
     domains: ['upcdn.io'],
   },
-  pwa: withPWA(),
 };
 
-if (shouldAnalyzeBundle) {
-  const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: true,
-  });
-  module.exports = withBundleAnalyzer(nextConfig);
-}
-
-module.exports = nextConfig;
-
-// module.exports = withBundleAnalyzer(
-//   withPWA({
-//     nextConfig
-//     nextConfig: {
-//       ...nextConfig,
-//     },
-//   })
-// );
-
-// module.exports = {
-//   ...nextConfig,
-// };
+module.exports = withBundleAnalyzer(withPWA(nextConfig));

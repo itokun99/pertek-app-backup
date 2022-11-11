4388a5748c1f3a8853150b7022e4dd1027b532b5
Muhammad Irva <muhammad.irva@bibit.id>
2022-11-12 05:44:41 +0700

    bugfix error build

--- src/components/dialog/FormStaff/FormStaff.interface.ts
+++ src/components/dialog/FormStaff/FormStaff.interface.ts
@@ -3,7 +3,7 @@ import { SelectOptionType } from "@types";
 
 interface IContactForm {
   contactId: string;
-  id: string;
+  id: number;
   firstName: string;
   lastName: string;
   identity: string;
